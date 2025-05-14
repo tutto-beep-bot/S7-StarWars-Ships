4d44699c742afba42f2578f215119eba2a3ea399
Davi Lessa <davilessa.br@gmail.com>
2025-05-14 21:10:56 +0200

    API implemented on service

--- src/app/services/starship.service.ts
+++ src/app/services/starship.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 interface StarShipApiResponse {
   results: {name: string; model: string;}[];
@@ -9,6 +10,11 @@ interface StarShipApiResponse {
   providedIn: 'root'
 })
 export class StarshipService {
+  private apiUrl = 'https://swapi.py4e.com/api/starships/'
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getStarShips(): Observable<StarShipApiResponse> {
+    return this.http.get<StarShipApiResponse>(this.apiUrl);
+  }
 }
