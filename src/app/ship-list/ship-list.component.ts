dc8f0f27e1772f2501167f699d7bf5104200af97
Davi Lessa <davilessa.br@gmail.com>
2025-05-19 11:13:38 +0200

    logo.component implemented and centered

--- src/app/ship-list/ship-list.component.ts
+++ src/app/ship-list/ship-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StarshipService } from '../services/starship.service';
+import { LogoComponent } from '../logo/logo.component';
 
 @Component({
   selector: 'app-ship-list',
-  imports: [],
+  imports: [LogoComponent],
   templateUrl: './ship-list.component.html',
   styleUrl: './ship-list.component.scss'
 })
