5be6858f156e6b9f3edc1e8f47ed95e5ffa33bb4
Davi Lessa <davilessa.br@gmail.com>
2025-05-20 19:02:00 +0200

    HomeComponent added and routing is now working from Home to ShipList

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { ShipDetailsComponent } from './ship-details/ship-details.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: ShipListComponent},
+    { path: '', component: HomeComponent},
     { path: 'ships/:id', component: ShipDetailsComponent},
     { path: 'home', component: HomeComponent},
     { path: 'starships', component: ShipListComponent}
