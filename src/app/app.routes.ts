7eecd95840364e30d9602d41b79075b7348d2189
Davi Lessa <davilessa.br@gmail.com>
2025-05-22 21:26:56 +0200

    login component  is now functional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,23 @@ import { Routes } from '@angular/router';
 import { ShipListComponent } from './ship-list/ship-list.component';
 import { ShipDetailsComponent } from './ship-details/ship-details.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { authGuard } from './guards/auth.guard';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'ships/:id', component: ShipDetailsComponent},
     { path: 'home', component: HomeComponent},
-    { path: 'starships', component: ShipListComponent},
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent}
+    { path: 'starships', component: ShipListComponent, canActivate: [authGuard]},
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./auth/login/login.component').then((m) => m.LoginComponent)
+    },     
+    {
+        path: 'register',
+        loadComponent: () =>
+            import('./auth/register/register.component').then((m) => m.RegisterComponent),
+  }
 ];
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,40 +3,39 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormControl, NonNullableFormBuilder } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  form: FormGroup<{
-    email: FormControl<string>;
-    password: FormControl<string>;
-  }>;
+  form!: FormGroup;
+  
     
 
-  constructor(private fb: NonNullableFormBuilder, private afAuth: AngularFireAuth, private router: Router) {
+  constructor(private fb: NonNullableFormBuilder, private afAuth: Auth, private router: Router) {}
+
+
+
+  ngOnInit() {
     this.form = this.fb.group({
-      email: this.fb.control ('', [Validators.required, Validators.email]),
-      password: this.fb.control('', Validators.required),
-  });
-  }
+      email: this.fb.control('', [Validators.required, Validators.email]),
+      password: this.fb.control('', Validators.required)
+    });
+  };
 
   login() {
-    if (this.form.valid) {
-      const { email, password } = this.form.getRawValue();
-
-      this.afAuth.signInWithEmailAndPassword(email, password)
-        .then(() => {
-          this.router.navigate(['/starships'])
-        })
-        .catch((error) => {
-          alert('Wrong data')
-        });
-    } else {
+    if (!this.form.valid) {
       this.form.markAllAsTouched();
+      return;
     }
+    const { email, password } = this.form.getRawValue();
+    signInWithEmailAndPassword(this.afAuth, email, password)
+      .then(() => this.router.navigate(['/starships']))
+      .catch(() => alert('Wrong credentials'));
   }
 }
