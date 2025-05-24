a2d386a5a7ebbcce1b0cbf182714541cd4241e24
Davi Lessa <davilessa.br@gmail.com>
2025-05-24 14:43:48 +0200

    rest of logout html was implemented.

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,24 +1,3 @@
-// import { inject } from '@angular/core';
-// import { CanActivateFn, Router } from '@angular/router';
-// import { AngularFireAuth } from '@angular/fire/compat/auth';
-// import { map, take } from 'rxjs/operators';
-
-// export const authGuard: CanActivateFn = (route, state) => {
-//     const afAuth = inject(AngularFireAuth);
-//     const router = inject(Router);
-
-//     return afAuth.authState.pipe(
-//       take(1),
-//       map(user => {
-//         if (!!user) {
-//           return true;
-//         } else {
-//           router.navigate(['/login']);
-//           return false;
-//       }
-//     })
-//   );
-// };
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { Auth, authState } from '@angular/fire/auth';
