13185f5c3396c424cf827a70af5928ae3c8d8e0f
Davi Lessa <davilessa.br@gmail.com>
2025-05-24 11:06:26 +0200

    login and signup buttons were implemented on Logo Component.

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-logo',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './logo.component.html',
   styleUrl: './logo.component.scss'
 })
