317dbae17cabc4237b77b4a84f75e0211fef8b6a
Davi Lessa <davilessa.br@gmail.com>
2025-05-26 11:36:40 +0200

    logic for interaction between home page and other components was improved to avoid being directed to login screen when already logged in

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Auth, onAuthStateChanged } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +10,20 @@ import { RouterModule } from '@angular/router';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isLoggedIn = false;
+
+  constructor(private router: Router, private auth: Auth) {
+    onAuthStateChanged(this.auth, (user) => {
+      this.isLoggedIn = !!user;
+    });
+  }
+
+  onStartClick(){
+    if(this.isLoggedIn) {
+      this.router.navigate(['/starships']);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
 
 }
